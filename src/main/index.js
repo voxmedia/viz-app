706c49e38bd84ddda396413ca485ee0a0a6af76a
Ryan Mark <ryan@mrk.cc>
2018-04-17 19:19:15 -0400

    enable autoupdate, setup release publishing to github, added license

--- src/main/index.js
+++ src/main/index.js
@@ -103,14 +103,8 @@ app.on('activate', () => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
 import { autoUpdater } from 'electron-updater'
 
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdatesAndNotify()
 })
- */
