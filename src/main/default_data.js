ca04a3d8cff445de885f0115cc8ee0caae6edd60
cwilliams <cwilliams@inquirer.com>
2019-07-19 13:55:00 -0400

    Removed new variable extraEmbedStylesheet, added stylesheet to custom template html file.

--- src/main/default_data.js
+++ src/main/default_data.js
@@ -22,7 +22,6 @@ const data = {
     "awsCloudfrontDistributionId": null,
     "extraPreviewCss": null,
     "extraEmbedCss": null,
-    "extraEmbedStylesheet": null,
     "ai2htmlFonts": null,
     "ai2htmlCredit": null,
     "oembedProviderName": null,
--- src/worker/tasks/project_deploy.js
+++ src/worker/tasks/project_deploy.js
@@ -45,16 +45,15 @@ function projectBuild({ project, settings }) {
     const embed_meta = getEmbedMeta(config)
     const extra_preview_css = settings.extraPreviewCss || ''
     const extra_embed_css = settings.extraEmbedCss || ''
-    const extra_embed_stylesheet = settings.extraEmbedStylesheet || ''
 
     fs.writeFile(
       path.join(dest, 'index.html'),
-      render('embed.html.ejs', { config, content, project, embed_meta, slug, deploy_url, extra_embed_css, extra_embed_stylesheet }),
+      render('embed.html.ejs', { config, content, project, embed_meta, slug, deploy_url, extra_embed_css }),
       end)
 
     fs.writeFile(
       path.join(dest, 'preview.html'),
-      render('preview.html.ejs', { config, embed_code, project, embed_meta, slug, deploy_url, extra_preview_css, extra_embed_stylesheet }),
+      render('preview.html.ejs', { config, embed_code, project, embed_meta, slug, deploy_url, extra_preview_css }),
       end)
 
     fs.writeFile(
